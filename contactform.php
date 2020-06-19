8adf545a33a594a3423c47858ff199a61a627ad0
varnnu <67059963+varnnu@users.noreply.github.com>
2020-06-19 13:32:33 +0530

    add php code to send email

--- contactform.php
+++ contactform.php
@@ -1,17 +1,63 @@
+
 <?php
-if(isset($_POST['submit'])){
-    $name= $_POST['name'];
-    $mailfrom= $_POST['email'];
-    $message= $_POST['message'];
+if(!isset($_POST['submit']))
+{
+	//This page should not be accessed directly. Need to submit the form.
+	echo "error; you need to submit the form!";
+}
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
 
-    $mailto="varnnu666@gmail.com";
-$headers="From: ".$mailfrom;
-$txt="you have recieved email from " .$name. ".\n\n".$message;
-
-mail($mailto,$headers, $txt);
-header("Location: thankyou.html?mailsend");
+//Validate first
+if(empty($name)||empty($email)) 
+{
+    echo "Name and email are mandatory!";
+    exit;
 }
 
+if(IsInjected($email))
+{
+    echo "Bad email value!";
+    exit;
+}
 
-?>
+$email_from = 'nimshamohammed@gmail.com';//<== update the email address
+$email_subject = "New Form submission";
+$email_body = "You have received a new message from the user $name.\n".
+    "Here is the message:\n $message".
+    
+$to = "nimshamohammed@gmail.com";//<== update the email address
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $email \r\n";
+//Send the email!
+mail($to,$email_subject,$email_body,$headers);
+//done. redirect to thank-you page.
+header('Location: thank-you.html');
+
+
+// Function to validate against any email injection attempts
+function IsInjected($str)
+{
+  $injections = array('(\n+)',
+              '(\r+)',
+              '(\t+)',
+              '(%0A+)',
+              '(%0D+)',
+              '(%08+)',
+              '(%09+)'
+              );
+  $inject = join('|', $injections);
+  $inject = "/$inject/i";
+  if(preg_match($inject,$str))
+    {
+    return true;
+  }
+  else
+    {
+    return false;
+  }
+}
+   
+?> 
 
